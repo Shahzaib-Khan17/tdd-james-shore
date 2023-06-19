a40b9436e774cf03aeb996619d58088637750074
Shahzaib Khan <SHamid@mobilehealth.com>
2023-06-20 02:23:04 +0500

    added new output message

--- finances/src/com/shahzaib/finances/Main.java
+++ finances/src/com/shahzaib/finances/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		SavingsAccount account = new SavingsAccount();
 		account.deposit(10000);
 		for (int i=0;i<60;i++) {
-			System.out.println(i+": $"+account.balance());
+			System.out.println(i+": $$"+account.balance());
 			account = account.nextYear(10);
 			
 		}
