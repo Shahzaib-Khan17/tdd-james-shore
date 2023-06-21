5e5ff474b1ac2a6b0682c08b5a395d51943c4f8c
Shahzaib Khan <SHamid@mobilehealth.com>
2023-06-21 16:47:10 +0500

    till 3rd lecture

--- finances/src/com/shahzaib/finances/_SavingsAccountTest.java
+++ finances/src/com/shahzaib/finances/_SavingsAccountTest.java
@@ -2,33 +2,33 @@ package com.shahzaib.finances;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class _SavingsAccountTest {
 
 	@Test
-	public void depositAndWithdrawl() {
-		SavingsAccount account = new SavingsAccount();
-		account.deposit(100);
-		assertEquals("After deposit",100,account.balance());
-		account.withdraw(50);
-		assertEquals("After withdrawl",50,account.balance());
+	public void startingBalance() {
+		SavingsAccountYear account = new SavingsAccountYear(10000,10);
+		assertEquals(10000,account.startingBalance());
 	}
 	
 	@Test
-	public void negativeBalanceIsJustFine() {
-		SavingsAccount account = new SavingsAccount();
-		account.withdraw(75);
-		assertEquals(-75,account.balance());
-	}
-	
-	@Test
-	public void nextYear() {
-		SavingsAccount account = new SavingsAccount();
-		account.deposit(10000);
-		SavingsAccount nextYear = account.nextYear(10);
-		assertEquals(11000,nextYear.balance());
-		
+	public void endingBalance() {
+		SavingsAccountYear account = new SavingsAccountYear(10000,10);
+		assertEquals(11000,account.endingBalance());
 	}
 
+	@Test
+	public void nextYearsStartingBalanceShouldEqualThisYearsEndingbalance() {
+		SavingsAccountYear thisYear = new SavingsAccountYear(10000,10);
+		assertEquals(thisYear.endingBalance(),thisYear.nextYear().startingBalance());
+	}
+	
+	@Test
+	public void nextYearsInterestRateEqualsThisYearsInterestRate() {
+		SavingsAccountYear thisYear = new SavingsAccountYear(10000,10);
+		assertEquals(thisYear.interestRate(),thisYear.nextYear().interestRate());
+	}
+	
 }
